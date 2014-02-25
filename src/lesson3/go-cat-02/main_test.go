ea2a0d50f4d9939c886fb4e38bddf37dea0f219c
Levi Cook <levicook@gmail.com>
2014-02-25 16:01:45 -0700

    go-cat Test_cat

--- src/lesson3/go-cat-02/main_test.go
+++ src/lesson3/go-cat-02/main_test.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 )
 
 func Test_cat(t *testing.T) {
+	src := strings.NewReader("foo")
+	dst := new(bytes.Buffer)
+
+	cat(src, dst)
+
+	expected := "foo"
+	observed := dst.String()
+
+	if expected != observed {
+		t.Errorf(`
+expected: %s
+observed: %s
+`, expected, observed)
+	}
+
 }
 
 func Test_determineSource(t *testing.T) {
--- src/lesson3/go-cat-03/main_test.go
+++ src/lesson3/go-cat-03/main_test.go
@@ -5,7 +5,22 @@ import (
 )
 
 func Test_cat(t *testing.T) {
+	src := strings.NewReader("foo")
+	dst := new(bytes.Buffer)
+
+	cat(src, dst)
+
+	expected := "foo"
+	observed := dst.String()
+
+	if expected != observed {
+		t.Errorf(`
+expected: %s
+observed: %s
+`, expected, observed)
+	}
+
 }
 
-func Test_determineInputs(t *testing.T) {
+func Test_determineSources(t *testing.T) {
 }
