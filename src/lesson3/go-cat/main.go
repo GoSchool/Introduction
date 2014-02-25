a3024915ca0289fcac2b0647d7c158d257640598
Levi Cook <levicook@gmail.com>
2014-02-25 11:57:12 -0700

    failOnError

--- src/lesson3/go-cat/main.go
+++ src/lesson3/go-cat/main.go
@@ -13,26 +13,23 @@ func main() {
 }
 
 func cat(src io.Reader, dst io.Writer) {
-	if _, err := io.Copy(dst, src); err != nil {
-		fail(err)
-	}
+	_, err := io.Copy(dst, src)
+	failOnError(err)
 }
 
 func determineSource(args []string) io.Reader {
 	if len(args) > 0 {
 		file, err := os.Open(args[0])
-
-		if err != nil {
-			fail(err)
-		}
-
+		failOnError(err)
 		return file
 	}
 
 	return os.Stdin
 }
 
-func fail(err error) {
-	println(err.Error())
-	os.Exit(1)
+func failOnError(err error) {
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
 }
