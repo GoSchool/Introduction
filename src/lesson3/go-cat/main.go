6fa6b9d964b8adecf5ec3469b74241e59b72b5a6
Levi Cook <levicook@gmail.com>
2014-02-25 10:29:27 -0700

    outlining go-cat

--- src/lesson3/go-cat/main.go
+++ src/lesson3/go-cat/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"io"
+	"os"
+)
+
 func main() {
-	println("hello")
+	cat(determineInputs(), os.Stdout)
+}
+
+func cat(inputs []io.Reader, output io.Writer) {
+	for _, input := range inputs {
+		if _, err := io.Copy(output, input); err != nil {
+			fail(err)
+		}
+	}
+}
+
+func determineInputs() (inputs []io.Reader) {
+	inputs = append(inputs, os.Stdin)
+
+	return
+}
+
+func fail(err error) {
+	println(err)
+	os.Exit(1)
 }
